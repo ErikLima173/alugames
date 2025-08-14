f6869475c709a74cfb903df9ec30a5e410105e5d
Erik Lima <134812021+ErikLima173@users.noreply.github.com>
2025-08-14 18:25:15 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,11 +3,12 @@ function alterarStatus(id) {
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
     let qtdJogosAlugados = document.querySelectorAll('.dashboard__item__img--rented');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
     console.log(qtdJogosAlugados.length + 1);
 
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        let resposta = confirm('Tem certeza que quer continuar?');
+        let resposta = confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`);
         if (!resposta) return;
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
